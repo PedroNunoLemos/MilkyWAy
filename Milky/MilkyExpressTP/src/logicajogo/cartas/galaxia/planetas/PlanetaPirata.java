dcd0a99b8b1f025d698c13eccf7649d2b0231726
PedroNunoLemos <pedro.nuno.f.lemos@gmail.com>
2015-08-13 22:45:07 +0100

    hgghj

--- Milky/MilkyExpressTP/src/logicajogo/cartas/galaxia/planetas/PlanetaPirata.java
+++ Milky/MilkyExpressTP/src/logicajogo/cartas/galaxia/planetas/PlanetaPirata.java
@@ -5,15 +5,21 @@ import java.util.HashMap;
 import java.util.Map;
 
 import logicajogo.cartas.Carta;
-import logicajogo.cubos.Cubo;
+import logicajogo.cubos.*;
 
 
 public abstract class PlanetaPirata extends Carta{
 	
-    String nome;
-    
-    Map<Cubo,Integer> precario = new HashMap<>();
-    
-    Cubo [] mercado = new Cubo[1];
+	  String nome;
+	    
+	    Map<Cubo,Integer> precario =
+	    		new HashMap<>();
+	    
+	    Cubo [] mercado = new Cubo[3];
+	    
+	    Cubo verificaCubos(){
+	        return null;
+	    }
+	    
 
 }
