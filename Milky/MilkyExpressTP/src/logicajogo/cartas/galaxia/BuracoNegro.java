11f9ec737a53f312ba1495f9301fd76eaf3bb6da
PedroNunoLemos <pedro.nuno.f.lemos@gmail.com>
2015-08-13 19:12:41 +0100

    dsadsa

---
+++ Milky/MilkyExpressTP/src/logicajogo/cartas/galaxia/BuracoNegro.java
@@ -0,0 +1,7 @@
+package logicajogo.cartas.galaxia;
+
+import logicajogo.cartas.Carta;
+
+public class BuracoNegro extends Carta {
+
+}
---
+++ Milky/MilkyExpressTP/src/logicajogo/cartas/galaxia/Vazio.java
@@ -0,0 +1,7 @@
+package logicajogo.cartas.galaxia;
+
+import logicajogo.cartas.Carta;
+
+public class Vazio extends Carta {
+
+}
